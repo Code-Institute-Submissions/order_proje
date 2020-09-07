cddf5ed750b3d3aad7c8f3f4f9312c072f06831c
macharia63 <julia.muthoni@hotmail.com>
2020-09-07 13:14:34 +0000

    Add card

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,25 @@
+/*
+    jQuery for MaterializeCSS initialization
+*/
 
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
+    $(".collapsible").collapsible();
 });
+
+
+
+
+
+
+
+
+
+/*
+    vanilla JavaScript for MaterializeCSS initialization
+*/
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     let sidenavs = document.querySelectorAll(".sidenav");
+//     let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
+// });
